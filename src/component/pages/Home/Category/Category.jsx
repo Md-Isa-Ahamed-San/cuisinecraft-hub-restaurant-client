27898ac3faba149dc02576e28b9b54115e818327
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-04-13 22:19:06 +0600

    carousal fixed

--- src/component/pages/Home/Category/Category.jsx
+++ src/component/pages/Home/Category/Category.jsx
@@ -11,11 +11,12 @@ import slide1 from "../../../../assets/home/slide1.jpg";
 import slide2 from "../../../../assets/home/slide2.jpg";
 import slide3 from "../../../../assets/home/slide3.jpg";
 import slide4 from "../../../../assets/home/slide4.jpg";
-import slide5 from "../../../../assets/home/slide5.jpg";
-import './Category.css'
+import slide5 from "../../../../assets/home/slide1.jpg";
+import "./Category.css";
 // import required modules
 import { FreeMode, Pagination } from "swiper/modules";
 import SectionIntro from "../../../common/SectionIntro";
+import { Link, NavLink } from "react-router-dom";
 const Category = () => {
   const breakpoints = {
     0: {
@@ -37,46 +38,30 @@ const Category = () => {
     <div>
       <SectionIntro heading={heading} text={text}></SectionIntro>
       <Swiper
-        // slidesPerView={slidesPerPage}
         breakpoints={breakpoints}
         spaceBetween={30}
         freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
+        pagination={{ clickable: true }}
         modules={[FreeMode, Pagination]}
         className="mySwiper max-w-7xl my-6 md:my-10 lg:my-20"
       >
-        <SwiperSlide>
-          <img src={slide1} alt="" />
-          <h3 className="font-bold text-center pr-5 text-xl md:text-3xl -mt-20 font-rancho ">
-            <span className="bg-gradient-to-r  from-green-500 to-[#d6b704] inline-block text-transparent bg-clip-text">SALAD</span>
-          </h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide2} alt="" />
-          <h3 className="font-bold text-center pr-5 text-xl md:text-3xl -mt-20 font-rancho text-[#FFD700]">
-          <span className="bg-gradient-to-r  from-green-500 to-[#d6b704] inline-block text-transparent bg-clip-text">PIZZA</span>
-          </h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide3} alt="" />
-          <h3 className="font-bold text-center pr-5 text-xl md:text-3xl -mt-20 font-rancho text-[#FFD700]">
-          <span className="bg-gradient-to-r  from-green-500 to-[#d6b704] inline-block text-transparent bg-clip-text">SOUP</span>
-          </h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide4} alt="" />
-          <h3 className="font-bold text-center pr-5 text-xl md:text-3xl -mt-20 font-rancho text-[#FFD700] ">
-          <span className="bg-gradient-to-r  from-green-500 to-[#d6b704] inline-block text-transparent bg-clip-text">DESSERT</span>
-          </h3>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide5} alt="" />
-          <h3 className="font-bold text-center pr-5 text-xl md:text-3xl font-rancho -mt-20 text-[#FFD700]">
-          <span className="bg-gradient-to-r  from-green-500 to-[#d6b704] inline-block text-transparent bg-clip-text">SALAD</span>
-          </h3>
-        </SwiperSlide>
+        {[slide1, slide2, slide3, slide4, slide5].map((img, index) => {
+          const titles = [{name:"SALAD",loc:"ourShop/salad"}, {name:"PIZZA",loc:"ourShop/pizza"}, {name:"SOUP",loc:"ourShop/soup"}, {name:"DESSERT",loc:"ourShop/dessert"}, {name:"SALAD",loc:"ourShop/salad"}];
+          return (
+            <SwiperSlide key={index}>
+              <div className="relative">
+                <img
+                  src={img}
+                  alt={titles[index]}
+                  className="w-full h-[250px] md:h-[300px] lg:h-[350px] object-cover rounded-xl shadow-lg"
+                />
+                <NavLink to={`/${titles[index].loc}`} className="absolute py-3 bottom-4 left-1/2 transform -translate-x-1/2 text-xl md:text-3xl font-rancho font-bold text-center bg-gradient-to-r from-green-500 to-[#d6b704] text-transparent bg-clip-text hover:text-yellow-400">
+                  {titles[index].name}
+                </NavLink>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
